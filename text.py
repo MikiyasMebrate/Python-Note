b7f59f8fae71fc0e1c22d313ce75c7765f94f971
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2023-04-07 07:07:04 +0300

    second largest number from list

--- text.py
+++ text.py
@@ -1,10 +1,14 @@
+import sys
+
 def seconda_largest(li):
-    first_largest = li[0]
-    second_largest = ""
+    first_largest = sys.maxsize * -1
+    second_largest = sys.maxsize * -1
     for i in li:
         if(i > first_largest):
             second_largest = first_largest
             first_largest = i
+        if(i < first_largest and i > second_largest):
+            second_largest = i
 
     print(first_largest)
     print(second_largest)
@@ -12,5 +16,5 @@ def seconda_largest(li):
 
 
 
-lstc = [2,3,5,676,4,3,7,8,500,700]       
+lstc = [900,3,5,676,4,800,7,8,1000,700]       
 seconda_largest(lstc)
